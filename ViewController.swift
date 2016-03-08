ae00426d064b82fbd99f9f40c285681ac69ba8f0
cwang427 <cjwang14@gmail.com>
2016-03-07 23:14:49 -0500

    Convert CGPoint to Millimeters
    Used dimensions of iPhone 6s Plus to display CGFloat distance as mm.
    Added app icons.

--- ViewController.swift
+++ ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var distanceLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -23,15 +25,22 @@ class ViewController: UIViewController {
     @IBAction func measurePointLong(sender: UILongPressGestureRecognizer) {
         if (sender.state == UIGestureRecognizerState.Began) {
             let touchPoint = sender.locationInView(self.view)
-            print("touched: \(touchPoint)")
             
             let originPoint = self.view.center
-            print("origin: \(originPoint)")
             
             let xDiff = (originPoint.x - touchPoint.x)
             let yDiff = (originPoint.y - touchPoint.y)
-            let distance = sqrt((pow(xDiff,2) + pow(yDiff,2)))
-            print("distance: \(distance)")
+            let distance = Double(sqrt((pow(xDiff,2) + pow(yDiff,2))))
+            
+            //Configured for iPhone 6s Plus with Retina HD 5.5 (1242 x 2208 pixels)
+            //3x scaling from point to pixel, 401 ppi
+            
+            let pixelDistance = distance * 3
+            let inchDistance = pixelDistance / 401
+            let mmDistance = inchDistance * 25.4
+            let roundedDistance = round(mmDistance * 100) / 100
+            
+            distanceLabel.text = "\(roundedDistance) mm"
         }
     }
 }
